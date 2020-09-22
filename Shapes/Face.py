a0af0a1053a9f6ca5eebff0e9a3e9274c45f52ff
Naimul Hoque <Naimul_Hoque@outlook.com>
2020-09-22 18:25:39 +0100

    Added type hints to Face

--- Shapes/Face.py
+++ Shapes/Face.py
@@ -1,23 +1,25 @@
+from typing import Tuple
+
 from Config import COLS, N, ROWS
 from Shapes.Square import Square
 
 
 class Face:
 
-    def __init__(self, name, color):
+    def __init__(self, name: str, color: Tuple[float, float, float]):
         self.squares = [Square(i, j, color) for i in range(ROWS) for j in range(COLS)]
         self.name = name
 
     def __str__(self):
         return self.name
 
-    def colors(self):
+    def colors(self) -> Tuple[Tuple[float, float, float]]:
         """
         returns a tuple of colors to use before
         as the 'before' movement when changing colors
         """
         return tuple(self.squares[i].color for i in range(N ** 2))
 
-    def GLDraw_Face(self, orientation):
+    def GLDraw_Face(self, orientation) -> None:
         for square in self.squares:
             square.GLDraw_Square(orientation)
