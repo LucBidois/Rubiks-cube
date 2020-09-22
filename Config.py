48505f526bb430ea2d2cff29fcecf593a9a4b6c3
Naimul Hoque <Naimul_Hoque@outlook.com>
2020-09-22 12:50:05 +0100

    Added config file containing game details

---
+++ Config.py
@@ -0,0 +1,15 @@
+# testing with 4x4 cube (works for nxn)
+n = 4
+rows = n
+cols = n
+
+# define colours (useful for pygame implementation)
+Orange = (1.0, 0.5, 0)  # (255, 130, 0) #Front
+white = (0.7, 0.7, 0.7)  # (255, 255, 255) #Bottom
+blue = (0, 0, 0.5)  # (0, 0, 255) # Right
+red = (0.5, 0, 0)  # (255, 0, 0) # Back
+green = (0, 0.5, 0)  # (0, 255, 0) # Left
+yellow = (0.7, 0.7, 0)  # (255, 255, 0) # Top
+
+black = (0, 0, 0)
+white = (255, 255, 255)
