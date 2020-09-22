d07e18f2d9891aff23b4ff5a61b84a9062a700db
Naimul Hoque <Naimul_Hoque@outlook.com>
2020-09-22 18:11:01 +0100

    Simplified constructor for Face by using list comprehension and removing color field

--- Shapes/Face.py
+++ Shapes/Face.py
@@ -2,23 +2,20 @@ from Config import COLS, N, ROWS
 from Shapes.Square import Square
 
 
-class Face():
+class Face:
 
     def __init__(self, name, color):
-        self.squares = []
+        self.squares = [Square(i, j, color) for i in range(ROWS) for j in range(COLS)]
         self.name = name
-        self.color = color
-        for i in range(ROWS):
-            for j in range(COLS):
-                self.squares.append(Square(i, j, self.color))
-        self.color = None  # removing attribute to ensure no confusion later
 
     def __str__(self):
         return self.name
 
     def colors(self):
-        """returns a tupled list of colors to use before
-        as the 'before' movement when changing colors"""
+        """
+        returns a tuple of colors to use before
+        as the 'before' movement when changing colors
+        """
         dummylist = []
         for i in range(0, N ** 2):
             dummylist.append(self.squares[i].color)
